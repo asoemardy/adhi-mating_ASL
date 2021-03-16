7b5d770e1d54a6ce80b8dbcb62413675c66150bc
unknown <tocheerfulashley@gmail.com>
2021-03-15 18:52:11 -0700

    update code

--- app folder/static/js/add_img.js
+++ app folder/static/js/add_img.js
@@ -1,12 +1,19 @@
 function add_uploads() {
     var uploads = d3.select('#uploads');
-    var input = uploads.append('input')
+
+    var all_uploads = $(".uploads").map(function() {
+        return $(this).val();
+    }).get();
+
+    if (all_uploads[all_uploads.length - 1] != '') {
+        var input = uploads.append('input')
         .attr('class', 'uploads')
         .attr('type', 'file')
         .attr('name', 'files[]')
         .attr('multiple', 'true')
         .attr('autocomplete', 'off')
         .text('new')
+    }
 }
 
 $(document).on('change', '.uploads', add_uploads)
