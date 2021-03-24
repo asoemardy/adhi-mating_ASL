b9311fad4bca36001474e8e941cb57b649f5e592
Falefor <68723120+asoemardy@users.noreply.github.com>
2021-03-23 21:52:10 -0700

    Update javascript

--- app folder/static/js/translate.js
+++ app folder/static/js/translate.js
@@ -1,9 +1,14 @@
-import * as tf from '@tensorflow/tfjs';
-const model = await tf.loadLayersModel('javascriptModel\model.json');
+const model = await tf.loadLayersModel("static/aslpredict/model.json");
 // JavaScript
 
-var pics = d3.select("#uploads")
-
-const example = tf.fromPixels(pics);  // for example
-const prediction = model.predict(example);
-
+var pics = d3.select(".uploadcolumn").select("img");
+var picture = new Image(64,64);
+picture.src = '/display/1.jpg'
+console.log(picture);
+const example = tf.browser.fromPixels(picture);  // for example
+console.log(example);
+var test = tf.expandDims(example);
+console.log(test);
+const prediction = model.predict(test);
+console.log(prediction);
+console.log(tf.argMax(prediction, -1));
